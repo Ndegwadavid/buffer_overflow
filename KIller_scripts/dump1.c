41cd94fb681468c49e863693274bf42ad281d529
Mercy waweru <wawerumercy105@gmail.com'wawerumercy105@gmail.com>
2023-06-05 22:26:43 +0300

    wth

---
+++ KIller_scripts/dump1.c
@@ -0,0 +1,16 @@
+/*
+vulnerable c code for testing buffer over flow vulnerability
+*/
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char *argv[])
+{
+char buffer[64];
+if (argc < 2)
+{
+printf("Error - You must supply at least one argument\n");
+}
+return 1;
+strcpy(buffer, argv[1]);
+return 0;
+}
